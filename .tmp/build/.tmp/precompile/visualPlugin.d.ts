0e81193094446042db8827dcb8fead51057982f5
Alexey <v-alesemenov@microsoft.com>
2025-11-16 13:55:36 +0100

    Remove .tmp folder and webpack.statistics files from git tracking

--- .tmp/build/.tmp/precompile/visualPlugin.d.ts
+++
@@ -1,4 +0,0 @@
-import powerbiVisualsApi from "powerbi-visuals-api";
-import IVisualPlugin = powerbiVisualsApi.visuals.plugins.IVisualPlugin;
-declare var canvasVisualBD042C189A134E6F914EF8053B29C0F9: IVisualPlugin;
-export default canvasVisualBD042C189A134E6F914EF8053B29C0F9;
